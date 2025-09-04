18a0b98a3903f3c5663906bdf38eafac7a683996
严炎 <1498984091@qq.com>
2025-09-05 00:17:29 +0800

    1
    注册页面

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import AuthImg from "@/public/Abstract Curves and Colors.jpeg";
+import { Logo } from "@/components/Logo";
+import AuthForm from "@/components/authentication/AuthForm";
 
-const page = () => {
+const AuthenticationPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <main className="h-screen grid grid-cols-2 relative">
+      <div className="relative w-full flex flex-col bg-muted p-10 text-primary-foreground">
+        <div className="w-full h-[30%] bg-gradient-to-t from-transparent to-black/50 absolute top-0 left-0 z-10" />
+        <div className="w-full h-[40%] bg-gradient-to-b from-transparent to-black/50 absolute bottom-0 left-0 z-10" />
+        <Image
+          src={AuthImg}
+          alt="login Image"
+          fill
+          className="w-full h-full object-cover"
+        />
+        <div className="relative z-20 flex items-center">
+          <Logo />
+        </div>
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              &ldquo;Pictoria AI is a game changer for me. I have been able to
+              generate high quality professional headshots within minutes. It
+              has saved me countless hours of work and cost as well.&rdquo;
+            </p>
+            <footer className="text-sm">David S.</footer>
+          </blockquote>
+        </div>
+      </div>
+      <div className="relative flex flex-col justify-center items-center p-8 h-full w-full">
+        <div className="max-w-xl mx-auto w-[350px]">
+            <AuthForm />
+            </div>
+      </div>
+    </main>
+  );
+};
 
-export default page
+export default AuthenticationPage;
