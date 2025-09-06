856538b570a2d4bafd7e9c09c4e225d2b89acba9
严炎 <1498984091@qq.com>
2025-09-07 01:36:11 +0800

    2
    图片生成页面

--- src/app/(dashboard)/image-generation/page.tsx
+++ src/app/(dashboard)/image-generation/page.tsx
@@ -1,8 +1,15 @@
+import Configurations from '@/components/image-generation/Configurations'
+import GeneratedImages from '@/components/image-generation/GeneratedImages'
 import React from 'react'
 
 const ImageGeneration = () => {
   return (
-    <div>ImageGeneration Page</div>
+    <section className='container mx-auto grid gap-4 grid-cols-3 overflow-hidden'>
+      <Configurations />
+      <div className='col-span-2 p-4 rounded-xl flex items-center justify-center h-fit'>
+        <GeneratedImages />
+      </div>
+    </section>
   )
 }
 
