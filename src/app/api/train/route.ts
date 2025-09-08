26728de84dcdf63f01b2e4a2a048fe46d0155b5d
严炎 <1498984091@qq.com>
2025-09-08 20:12:58 +0800

    5
    通量模型训练

---
+++ src/app/api/train/route.ts
@@ -0,0 +1,106 @@
+import { supabaseAdmin } from "@/lib/supabase/admin";
+import { createClient } from "@/lib/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    if (!process.env.REPLICATE_API_TOKEN) {
+      throw new Error("The replicate api token is not set!");
+    }
+
+    const supabase = await createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized",
+        },
+        { status: 401 }
+      );
+    }
+
+    const formData = await request.formData();
+    const input = {
+      fileKey: formData.get("fileKey") as string,
+      modelName: formData.get("modelName") as string,
+      gender: formData.get("gender") as string,
+    };
+
+    if (!input.fileKey || !input.modelName) {
+      return NextResponse.json(
+        { error: "Missing required fields!" },
+        { status: 400 }
+      );
+    }
+
+    const fileName = input.fileKey.replace("training_data/", "");
+    const { data: fileUrl } = await supabaseAdmin.storage
+      .from("training_data")
+      .createSignedUrl(fileName, 3600);
+
+    if (!fileUrl?.signedUrl) {
+      throw new Error("Failed to get the file URL");
+    }
+
+    // const hardware = await replicate.hardware.list();
+
+    // console.log(hardware)
+
+    const modelId = `${user.id}_${Date.now()}_${input.modelName
+      .toLowerCase()
+      .replaceAll(" ", "_")}`;
+
+    // // create model first
+    await replicate.models.create("yanshiyu1", modelId, {
+      visibility: "private",
+      hardware: "gpu-a100-large",
+    });
+
+    // start training
+    const training = await replicate.trainings.create(
+      "ostris",
+      "flux-dev-lora-trainer",
+      "26dce37af90b9d997eeb970d92e47de3064d46c300504ae376c75bef6a9022d2",
+      {
+        // You need to create a model on Replicate that will be the destination for the trained version.
+        destination: `yanshiyu1/${modelId}`,
+        input: {
+          steps: 1200,
+          resolution: "1024",
+          input_images: fileUrl.signedUrl,
+          trigger_word: "ohwx",
+        },
+      }
+    );
+
+    console.log(training);
+
+    return NextResponse.json(
+      {
+        success: true,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Training Error: ", error);
+
+    const errorMessage =
+      error instanceof Error
+        ? error.message
+        : "Failed to start the model training!";
+
+    return NextResponse.json(
+      {
+        error: errorMessage,
+      },
+      { status: 500 }
+    );
+  }
+}
