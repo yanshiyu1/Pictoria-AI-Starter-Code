4d7edc21e9f52ca7988d3750f8593632fb776395
严炎 <1498984091@qq.com>
2025-09-14 23:38:09 +0800

    1

--- src/components/models/ModelsList.tsx
+++ src/components/models/ModelsList.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
-const ModelsList = () => {
+type ModelType = {
+  error: string | null,
+  success: boolean,
+  data: 
+}
+
+interface ModelsListProps {
+  models: ModelType
+}
+
+const ModelsList = ({ models }: ModelsListProps) => {
   return (
     <div>ModelsList</div>
   )
